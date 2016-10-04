cc4f6d522cf7de287a06027f2de542b80a91379d
sean.yangyh <seanis123@gmail.com>
2016-10-04 19:00:13 +0800

    modify py via IDE Anaconda suggestion.

--- KBD_regression_test.py
+++ KBD_regression_test.py
@@ -1,14 +1,8 @@
-import os, time
-#import sys, fnmatch, smtplib, shutil, subprocess
-#from time import sleep
-#from datetime import date
+import os
+import time
+import subprocess
 import unittest
 from appium import webdriver
-
-#from selenium.webdriver.common.by import By
-#from selenium.webdriver.support.ui import WebDriverWait
-#from selenium.webdriver.support import expected_conditions as EC
-#from selenium.common.exceptions import TimeoutException
 
 import KBD_util as ul
 import KBD_element as el
@@ -20,6 +14,7 @@ PATH = lambda p: os.path.abspath(os.path.join(os.path.dirname(__file__), p))
 # Result = dict (Manufacturer,Model,Brand,Androidversion,SDKversion,SerialNo)
 Result = ul.device_registration.getDeviceStatus()
 
+
 class KBD_regression_test(unittest.TestCase):
     def setUp(self):
         desired_caps = {}
@@ -30,122 +25,106 @@ class KBD_regression_test(unittest.TestCase):
         desired_caps['appPackage'] = 'com.ijinshan.kbatterydoctor_en'
 
         self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
-        self.kbdutil = ul.Util(self.driver,os.getcwd()+"/screenshots")
-    
+        self.kbdutil = ul.Util(self.driver, os.getcwd() + "/screenshots")
+
     def tearDown(self):
-        #end the session
         self.driver.quit()
-    
+
     def test_SaveTabOptimize(self):
         getDrainnumA = ""
-        #getDrainnumB = ""
         ResultPageFCText = ""
         HomePagePercent = ""
 
-        #Click Save Tab
-        if self.kbdutil.checkElClickable(el.BatteryDoctor["SaveTab"]) == True:
+        if self.kbdutil.checkElClickable(el.BatteryDoctor["SaveTab"]) is True:
             self.kbdutil.clickEl(el.BatteryDoctor["SaveTab"])
             print("[PASS][Home Page]Check Save Tab done.")
         else:
-            self.assertTrue(False,"[FAIL][Home Page]Check Save Tab fail.")
+            self.assertTrue(False, "[FAIL][Home Page]Check Save Tab fail.")
 
-        #Get draining apps count A (Home page)
-        if self.kbdutil.checkElVisible(el.SaveTab["DrainnumA"]) == True:
+        if self.kbdutil.checkElVisible(el.SaveTab["DrainnumA"]) is True:
             getDrainnumA = self.kbdutil.getTextEL(el.SaveTab["DrainnumA"])
             print("[PASS][Home Page]Get Home Page Drain num A done. getDrainnumA = " + str(getDrainnumA))
         else:
-            self.assertTrue(False,"[FAIL][Home Page]Get Home Page Drain num A fail.")
+            self.assertTrue(False, "[FAIL][Home Page]Get Home Page Drain num A fail.")
 
-        #Click Save Tab Optimize button
-        if self.kbdutil.checkElClickable(el.SaveTab["Optimize"]) == True:
+        if self.kbdutil.checkElClickable(el.SaveTab["Optimize"]) is True:
             self.kbdutil.clickEl(el.SaveTab["Optimize"])
             print("[PASS][Home Page]Click Optimize button done.")
         else:
-            self.assertTrue(False,"[FAIL][Home Page]Click Optimize button fail.")
-        
-        #Check and Click X of floating window if need
-        if self.kbdutil.checkElClickable(el.ResultPage["PopupWindowTryitnow"]) == True:
+            self.assertTrue(False, "[FAIL][Home Page]Click Optimize button fail.")
+
+        if self.kbdutil.checkElClickable(el.ResultPage["PopupWindowTryitnow"]) is True:
             self.kbdutil.clickEl(el.ResultPage["PopupWindowTryitnow"])
             print("[PASS][Result Page]Check and Click floating window Tryitnow done. It's 1st time enter to Result Page.")
         else:
             print("[PASS][Result Page]Floating window didn't exist. It's 2nd time above enter to Result Page.")
 
-        #Make sure KBD in Result Page
-        if self.kbdutil.checkElVisible(el.ResultPage["FisrtCardText"]) == True:
+        if self.kbdutil.checkElVisible(el.ResultPage["FisrtCardText"]) is True:
             ResultPageFCText = self.kbdutil.getTextEL(el.ResultPage["FisrtCardText"])
-            #ResultPageFC = self.kbdutil.waitUntilAndGetElement("id",el.ResultPage["FisrtCardText"])
-            self.assertTrue(True,"[PASS][Result Page]Get First Card Text done. ResultPageFCText = " + str(ResultPageFCText))
+            self.assertTrue(True, "[PASS][Result Page]Get First Card Text done. ResultPageFCText = " + str(ResultPageFCText))
         else:
-            self.assertTrue(False,"[FAIL][Result Page]Get Result Page First Card Text fail.")
-        
-        #Press Back to Home page
-        if self.kbdutil.checkElClickable(el.ResultPage["Backkey"]) == True:
+            self.assertTrue(False, "[FAIL][Result Page]Get Result Page First Card Text fail.")
+
+        if self.kbdutil.checkElClickable(el.ResultPage["Backkey"]) is True:
             self.kbdutil.clickEl(el.ResultPage["Backkey"])
             print("[PASS][Result Page]Click Back key to Home done.")
         else:
-            self.assertTrue(False,"[FAIL][Result Page]Click Back key to Home fail.")
+            self.assertTrue(False, "[FAIL][Result Page]Click Back key to Home fail.")
 
-        #Check and Click X of Home floating window if need
-        if self.kbdutil.checkElClickable(el.SaveTab["FloatingX"]) == True:
+        if self.kbdutil.checkElClickable(el.SaveTab["FloatingX"]) is True:
             self.kbdutil.clickEl(el.SaveTab["FloatingX"])
             print("[PASS][Home Page]Check and Click floating window X done. It's 1st time enter to Home Page.")
         else:
             print("[PASS][Home Page]Floating window X didn't exist. It's 2nd time above enter to Home Page.")
 
-        #Check test case back to KBD Home successfully (Percentage show up)
-        if self.kbdutil.checkElVisible(el.SaveTab["Percent"]) == True:
+        if self.kbdutil.checkElVisible(el.SaveTab["Percent"]) is True:
             HomePagePercent = self.kbdutil.getTextEL(el.SaveTab["Percent"])
             print("[PASS][Home Page]Get Battery Percentage done. HomePagePercent = " + str(HomePagePercent))
         else:
-            self.assertTrue(False,"[FAIL][Home Page]Get Battery Percentage fail.")
+            self.assertTrue(False, "[FAIL][Home Page]Get Battery Percentage fail.")
 
     def test_SaveTabHWbutton(self):
-            
-        #Click Save Tab
-        if self.kbdutil.checkElClickable(el.BatteryDoctor["SaveTab"]) == True:
+        if self.kbdutil.checkElClickable(el.BatteryDoctor["SaveTab"]) is True:
             self.kbdutil.clickEl(el.BatteryDoctor["SaveTab"])
             print("[PASS][Home Page]Check Save Tab done.")
         else:
-            self.assertTrue(False,"[FAIL][Home Page]Check Save Tab fail.")
+            self.assertTrue(False, "[FAIL][Home Page]Check Save Tab fail.")
 
-        #Check test case back to KBD Home successfully (Percentage show up)
-        if self.kbdutil.checkElVisible(el.SaveTab["Percent"]) == True:
+        if self.kbdutil.checkElVisible(el.SaveTab["Percent"]) is True:
             HomePagePercent = self.kbdutil.getTextEL(el.SaveTab["Percent"])
             print("[PASS][Home Page]Get Battery Percentage done. HomePagePercent = " + str(HomePagePercent))
         else:
-            self.assertTrue(False,"[FAIL][Home Page]Get Battery Percentage fail.")
+            self.assertTrue(False, "[FAIL][Home Page]Get Battery Percentage fail.")
 
-        #Scroll to Wifi Switch button
-        if self.kbdutil.scrollTo(el.SaveTab["SwitchWifi"]) == True:
+        if self.kbdutil.scrollTo(el.SaveTab["SwitchWifi"]) is True:
             print("[PASS][Home Page]Scroll to Get Switch WiFi button done.")
         else:
-            self.assertTrue(False,"[FAIL][Home Page]Scroll to Get Switch WiFi button fail.")
+            self.assertTrue(False, "[FAIL][Home Page]Scroll to Get Switch WiFi button fail.")
 
-        #Test WiFi button
-        if self.kbdutil.checkElVisible(el.SaveTab["SwitchWifi"]) == True:
-            subprocess.getoutput("adb shell am start -n com.android.settings/.wifi.WifiStatusTest")
+        if self.kbdutil.checkElVisible(el.SaveTab["SwitchWifi"]) is True:
+            subprocess.call("adb shell am start -n com.android.settings/.wifi.WifiStatusTest")
             WiFiStatusA = self.kbdutil.getTextEL(el_other.WifiStatusTest["State"])
             self.driver.back()
             self.kbdutil.clickEl(el.SaveTab["SwitchWifi"])
-            subprocess.getoutput("adb shell am start -n com.android.settings/.wifi.WifiStatusTest")
+            subprocess.call("adb shell am start -n com.android.settings/.wifi.WifiStatusTest")
             WiFiStatusB = self.kbdutil.getTextEL(el_other.WifiStatusTest["State"])
             if WiFiStatusB == "Disabled":
                 self.kbdutil.clickEl(el.SaveTab["SwitchWifi"])
             print("[PASS][Home Page]Test Switch WiFi button done.")
         else:
-            self.assertTrue(False,"[FAIL][Home Page]Test Switch WiFi button fail.")
+            self.assertTrue(False, "[FAIL][Home Page]Test Switch WiFi button fail.")
 
     def test_firstExecute_have_rating(self):
         try:
-            savePowerEle = self.kbdutil.waitUntilAndGetElement("id",el.BatteryDoctor["SaveTab"],20)
+            savePowerEle = self.kbdutil.waitUntilAndGetElement("id", el.BatteryDoctor["SaveTab"], 20)
             savePowerEle.click()
-            oneTapSavePowerBtnEle = self.kbdutil.waitUntilAndGetElement("id",el.SaveTab["Optimize"])
+            oneTapSavePowerBtnEle = self.kbdutil.waitUntilAndGetElement("id", el.SaveTab["Optimize"])
             oneTapSavePowerBtnEle.click()
-            guide_btn_close = self.kbdutil.waitUntilAndGetElement("id",el.ResultPage["PopupWindowX"],5)
+            guide_btn_close = self.kbdutil.waitUntilAndGetElement("id", el.ResultPage["PopupWindowX"], 5)
             guide_btn_close.click()
-            backEle = self.kbdutil.waitUntilAndGetElement("id",el.ResultPage["Backkey"])
+            backEle = self.kbdutil.waitUntilAndGetElement("id", el.ResultPage["Backkey"])
             backEle.click()
-            if self.kbdutil.checkElVisible(el.SaveTab["FloatingFull"]) == True:
+            if self.kbdutil.checkElVisible(el.SaveTab["FloatingFull"]) is True:
                 self.assertTrue(True)
         except:
             self.assertTrue(False)
@@ -155,6 +134,6 @@ if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(KBD_regression_test)
     #unittest.TextTestRunner(verbosity=2).run(suite)
     file = open(str(PATH('./KBD_result/' + str(time.strftime("%Y%m%d") + '.html'))) ,"wb")
-    runner = HTMLTestRunner.HTMLTestRunner(stream=file,title="[KBD Automation] [Python+Appium] [Device: " + ' ' + Result["Manufacturer"] + ' ' + Result["Model"] + ' ' + Result["Brand"] + ']',description="[Platform Version: " + Result["Androidversion"] + ']' + '\n' + "[SDK version: " + Result["SDKversion"] + ']' + '\n' + "[Device S/N: " + Result["SerialNo"] + ']')
+    runner = HTMLTestRunner.HTMLTestRunner(stream=file, title="[KBD Automation] [Python+Appium] [Device: " + ' ' + Result["Manufacturer"] + ' ' + Result["Model"] + ' ' + Result["Brand"] + ']', description="[Platform Version: " + Result["Androidversion"] + ']' + '\n' + "[SDK version: " + Result["SDKversion"] + ']' + '\n' + "[Device S/N: " + Result["SerialNo"] + ']')
     runner.run(suite)
     file.close()
