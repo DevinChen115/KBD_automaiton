7199160e79ac9dfd20780066caa6f85f5d5841d7
sean.yangyh <seanis123@gmail.com>
2016-10-05 12:16:16 +0800

    KBD_Automation_v0.65, 1. remove SaveTabOptimize case, 2. SaveTabHWButton last modified date 2016/10/5.

--- KBD_element.py
+++ KBD_element.py
@@ -22,7 +22,7 @@ BatteryDoctor["RankTab"] = "com.ijinshan.kbatterydoctor_en:id/radio_button3"
 #/Weather card
 """
 SaveTab = {}
-#/Main Fuction
+# /Main Fuction
 SaveTab["Title"] = "com.ijinshan.kbatterydoctor_en:id/name"
 SaveTab["GiftBox"] = "com.ijinshan.kbatterydoctor_en:id/jump_url_img_default"
 SaveTab["Menu"] = "com.ijinshan.kbatterydoctor_en:id/action_img_layout"
@@ -31,43 +31,44 @@ SaveTab["PhoneStatus"] = "com.ijinshan.kbatterydoctor_en:id/tv_phone_state"
 SaveTab["DrainnumA"] = "com.ijinshan.kbatterydoctor_en:id/tv_phone_issues"
 SaveTab["Remaining"] = "com.ijinshan.kbatterydoctor_en:id/remaining_time_area"
 SaveTab["Optimize"] = "com.ijinshan.kbatterydoctor_en:id/btn_detection"
-#/AD
+# /AD
 SaveTab["AD"] = "com.ijinshan.kbatterydoctor_en:id/context_layout"
-#/Locker promote card
+# /Locker promote card
 SaveTab["Lockerpromotecard"] = ""
-#/App usage info
+# /App usage info
 SaveTab["AppUsageItem1"] = "com.ijinshan.kbatterydoctor_en:id/drain_app_layout0"
 SaveTab["AppUsageItem2"] = "com.ijinshan.kbatterydoctor_en:id/drain_app_layout1"
-#/Power Remaining
+# /Power Remaining
 SaveTab["PowerRemaining"] = "com.ijinshan.kbatterydoctor_en:id/call_3g_layout"
-#/HW button
+# /HW button
 SaveTab["SwitchWifi"] = "com.ijinshan.kbatterydoctor_en:id/shortcut_linear_wifi"
 SaveTab["SwitchBT"] = "com.ijinshan.kbatterydoctor_en:id/shortcut_linear_bluetooth"
 SaveTab["SwitchScreenLight"] = "com.ijinshan.kbatterydoctor_en:id/shortcut_linear_screen"
 SaveTab["SwitchScreenTimeout"] = "com.ijinshan.kbatterydoctor_en:id/shortcut_linear_screen_timeout"
 SaveTab["SwitchVolume"] = "com.ijinshan.kbatterydoctor_en:id/shortcut_linear_volume"
-#/Battery History
+# /Battery History
 SaveTab["BatteryHistory"] = "com.ijinshan.kbatterydoctor_en:id/batteryInfoGraph"
-#/Temperature Voltage Capacity
+# /Temperature Voltage Capacity
 SaveTab["Temperature"] = "com.ijinshan.kbatterydoctor_en:id/tv_bottom_info_temp_value"
 SaveTab["Voltage"] = "com.ijinshan.kbatterydoctor_en:id/tv_bottom_info_voltage_value"
 SaveTab["Capacity"] = "com.ijinshan.kbatterydoctor_en:id/tv_bottom_info_tech_value"
-#/Weather card
+# /Weather card
 SaveTab["WeatherCard"] = "com.ijinshan.kbatterydoctor_en:id/card_content"
 SaveTab["WeatherCity"] = "com.ijinshan.kbatterydoctor_en:id/title_tv"
 SaveTab["WeatherToday"] = "com.ijinshan.kbatterydoctor_en:id/weather_now"
 SaveTab["WeatherTomorrow"] = "com.ijinshan.kbatterydoctor_en:id/weather_tomorrow"
-#/Floating window
+# /Floating window
 SaveTab["FloatingFull"] = "com.ijinshan.kbatterydoctor_en:id/rating_body"
 SaveTab["FloatingX"] = "com.ijinshan.kbatterydoctor_en:id/rating_icon_close"
-SaveTab["FloatingTitle"] = "com.ijinshan.kbatterydoctor_en:id/gp_shocks" #Congratulations!
-SaveTab["FloatingSubTitle"] = "com.ijinshan.kbatterydoctor_en:id/summary" #Extended 8 m successfully
-SaveTab["FloatingAskStar"] = "com.ijinshan.kbatterydoctor_en:id/ask_for_stars" #Impressed? Is that worth a 5-star rating?
-SaveTab["FloatingComplainButton"] = "com.ijinshan.kbatterydoctor_en:id/button_feedback" #COMPLAIN
-SaveTab["FloatingRate5starButton"] = "com.ijinshan.kbatterydoctor_en:id/button" #RATE 5-STARS
+SaveTab["FloatingTitle"] = "com.ijinshan.kbatterydoctor_en:id/gp_shocks"  # Congratulations!
+SaveTab["FloatingSubTitle"] = "com.ijinshan.kbatterydoctor_en:id/summary"  # Extended 8 m successfully
+SaveTab["FloatingAskStar"] = "com.ijinshan.kbatterydoctor_en:id/ask_for_stars"  # Impressed? Is that worth a 5-star rating?
+SaveTab["FloatingComplainButton"] = "com.ijinshan.kbatterydoctor_en:id/button_feedback"  # COMPLAIN
+SaveTab["FloatingRate5starButton"] = "com.ijinshan.kbatterydoctor_en:id/button"  # RATE 5-STARS
 SaveTab["FloatingOutsideArea"] = "com.ijinshan.kbatterydoctor_en:id/dialog_outside_layout"
-#/AvailableTime
+# /AvailableTime
 SaveTab["AvailableTimeMovie"] = "com.ijinshan.kbatterydoctor_en:id/play_movie_icon"
+
 
 """
 [Scanning Page] 1st Tab Save Tab press Optimize then go to scanning page.
@@ -91,29 +92,29 @@ ScanPage["DrainnumText"] = "com.ijinshan.kbatterydoctor_en:id/optimize_software_
 #/AD
 """
 ResultPage = {}
-#/Pop up Window to add shortcut
+# /Pop up Window to add shortcut
 ResultPage["PopupWindowX"] = "com.ijinshan.kbatterydoctor_en:id/guide_btn_close"
-ResultPage["PopupWindowText"] = "com.ijinshan.kbatterydoctor_en:id/guide_app_caption" #Create a 1 Tap shortcut to use SAVE POWER to extend your battery easily.
+ResultPage["PopupWindowText"] = "com.ijinshan.kbatterydoctor_en:id/guide_app_caption"  # Create a 1 Tap shortcut to use SAVE POWER to extend your battery easily.
 ResultPage["PopupWindowNo"] = "com.ijinshan.kbatterydoctor_en:id/guide_btn_no"
 ResultPage["PopupWindowTryitnow"] = "com.ijinshan.kbatterydoctor_en:id/guide_btn_try_it"
-#/Title
+# /Title
 ResultPage["Backkey"] = "com.ijinshan.kbatterydoctor_en:id/opt_action_back"
-ResultPage["TitleText"] = "com.ijinshan.kbatterydoctor_en:id/opt_title" #Save Power
+ResultPage["TitleText"] = "com.ijinshan.kbatterydoctor_en:id/opt_title"  # Save Power
 ResultPage["Menu"] = "com.ijinshan.kbatterydoctor_en:id/opt_action_more"
-#/Fisrt card
-ResultPage["FisrtCardText"] = "com.ijinshan.kbatterydoctor_en:id/tip" #Remaining time  21h 33m
-#/AD/FB
+# /Fisrt card
+ResultPage["FisrtCardText"] = "com.ijinshan.kbatterydoctor_en:id/tip"  # Remaining time  21h 33m
+# /AD/FB
 ResultPage["ADFB"] = "com.ijinshan.kbatterydoctor_en:id/cmcm_fb_result_main_mix_layout"
-ResultPage["ADFBTopTitle"] = "com.ijinshan.kbatterydoctor_en:id/cmcm_fb_top_title" #Recommended for you
-ResultPage["ADFBTitle"] = "com.ijinshan.kbatterydoctor_en:id/cmcm_fb_app_title" #UpLive
-ResultPage["ADFBButton"] = "com.ijinshan.kbatterydoctor_en:id/cmcm_fb_app_btn" #Install Now
-ResultPage["ADFBDownloadnum"] = "com.ijinshan.kbatterydoctor_en:id/cmcm_fb_app_downloads" #FREE · 544,625 people use this
+ResultPage["ADFBTopTitle"] = "com.ijinshan.kbatterydoctor_en:id/cmcm_fb_top_title"  # Recommended for you
+ResultPage["ADFBTitle"] = "com.ijinshan.kbatterydoctor_en:id/cmcm_fb_app_title"  # UpLive
+ResultPage["ADFBButton"] = "com.ijinshan.kbatterydoctor_en:id/cmcm_fb_app_btn"  # Install Now
+ResultPage["ADFBDownloadnum"] = "com.ijinshan.kbatterydoctor_en:id/cmcm_fb_app_downloads"  # FREE · 544,625 people use this
 ResultPage["ADFBDescribe"] = "com.ijinshan.kbatterydoctor_en:id/cmcm_fb_app_desc"
-ResultPage["ADFBSubscript"] = "com.ijinshan.kbatterydoctor_en:id/cmcm_fb_app_common_tag" #角標
-#/Super Optimize
-ResultPage["SuperCardTitle"] = "com.ijinshan.kbatterydoctor_en:id/title" #SUPER OPTIMIZE
-ResultPage["SuperCardText"] = "com.ijinshan.kbatterydoctor_en:id/description" #Force stop background apps
-ResultPage["SuperCardButton"] = "com.ijinshan.kbatterydoctor_en:id/button" #LAUNCH
+ResultPage["ADFBSubscript"] = "com.ijinshan.kbatterydoctor_en:id/cmcm_fb_app_common_tag"  # 角標
+# /Super Optimize
+ResultPage["SuperCardTitle"] = "com.ijinshan.kbatterydoctor_en:id/title"  # SUPER OPTIMIZE
+ResultPage["SuperCardText"] = "com.ijinshan.kbatterydoctor_en:id/description"  # Force stop background apps
+ResultPage["SuperCardButton"] = "com.ijinshan.kbatterydoctor_en:id/button"  # LAUNCH
 
 Availabletime = {}
 Availabletime['2GIcon'] = "com.ijinshan.kbatterydoctor_en:id/call_2g_icon"
--- KBD_regression_test.py
+++ KBD_regression_test.py
@@ -10,7 +10,13 @@ import Other_element as el_other
 import HTMLTestRunner
 
 # Returns abs path relative to this file and not cwd
-PATH = lambda p: os.path.abspath(os.path.join(os.path.dirname(__file__), p))
+# PATH = lambda p: os.path.abspath(os.path.join(os.path.dirname(__file__), p))
+
+
+def PATH(p):
+    return os.path.abspath(os.path.join(os.path.dirname(__file__), p))
+
+
 # Result = dict (Manufacturer,Model,Brand,Androidversion,SDKversion,SerialNo)
 Result = ul.device_registration.getDeviceStatus()
 
@@ -30,118 +36,56 @@ class KBD_regression_test(unittest.TestCase):
     def tearDown(self):
         self.driver.quit()
 
-    def test_SaveTabOptimize(self):
-        getDrainnumA = ""
-        ResultPageFCText = ""
-        HomePagePercent = ""
+    def test_SaveTab_HWbutton(self):
+        try:
+            savePowerEle = self.kbdutil.waitUntilAndGetElement("id", el.BatteryDoctor["SaveTab"], "ClickSaveTab", 20)
+            savePowerEle.click()
+            HWButtonWiFi = self.kbdutil.scrollUntilGetElement("id", el.SaveTab["SwitchWifi"], "ClickSwitchWiFiButton")
+            HWButtonWiFi.click()
 
-        if self.kbdutil.checkElClickable(el.BatteryDoctor["SaveTab"]) is True:
-            self.kbdutil.clickEl(el.BatteryDoctor["SaveTab"])
-            print("[PASS][Home Page]Check Save Tab done.")
-        else:
-            self.assertTrue(False, "[FAIL][Home Page]Check Save Tab fail.")
-
-        if self.kbdutil.checkElVisible(el.SaveTab["DrainnumA"]) is True:
-            getDrainnumA = self.kbdutil.getTextEL(el.SaveTab["DrainnumA"])
-            print("[PASS][Home Page]Get Home Page Drain num A done. getDrainnumA = " + str(getDrainnumA))
-        else:
-            self.assertTrue(False, "[FAIL][Home Page]Get Home Page Drain num A fail.")
-
-        if self.kbdutil.checkElClickable(el.SaveTab["Optimize"]) is True:
-            self.kbdutil.clickEl(el.SaveTab["Optimize"])
-            print("[PASS][Home Page]Click Optimize button done.")
-        else:
-            self.assertTrue(False, "[FAIL][Home Page]Click Optimize button fail.")
-
-        if self.kbdutil.checkElClickable(el.ResultPage["PopupWindowTryitnow"]) is True:
-            self.kbdutil.clickEl(el.ResultPage["PopupWindowTryitnow"])
-            print("[PASS][Result Page]Check and Click floating window Tryitnow done. It's 1st time enter to Result Page.")
-        else:
-            print("[PASS][Result Page]Floating window didn't exist. It's 2nd time above enter to Result Page.")
-
-        if self.kbdutil.checkElVisible(el.ResultPage["FisrtCardText"]) is True:
-            ResultPageFCText = self.kbdutil.getTextEL(el.ResultPage["FisrtCardText"])
-            self.assertTrue(True, "[PASS][Result Page]Get First Card Text done. ResultPageFCText = " + str(ResultPageFCText))
-        else:
-            self.assertTrue(False, "[FAIL][Result Page]Get Result Page First Card Text fail.")
-
-        if self.kbdutil.checkElClickable(el.ResultPage["Backkey"]) is True:
-            self.kbdutil.clickEl(el.ResultPage["Backkey"])
-            print("[PASS][Result Page]Click Back key to Home done.")
-        else:
-            self.assertTrue(False, "[FAIL][Result Page]Click Back key to Home fail.")
-
-        if self.kbdutil.checkElClickable(el.SaveTab["FloatingX"]) is True:
-            self.kbdutil.clickEl(el.SaveTab["FloatingX"])
-            print("[PASS][Home Page]Check and Click floating window X done. It's 1st time enter to Home Page.")
-        else:
-            print("[PASS][Home Page]Floating window X didn't exist. It's 2nd time above enter to Home Page.")
-
-        if self.kbdutil.checkElVisible(el.SaveTab["Percent"]) is True:
-            HomePagePercent = self.kbdutil.getTextEL(el.SaveTab["Percent"])
-            print("[PASS][Home Page]Get Battery Percentage done. HomePagePercent = " + str(HomePagePercent))
-        else:
-            self.assertTrue(False, "[FAIL][Home Page]Get Battery Percentage fail.")
-
-    def test_SaveTabHWbutton(self):
-        if self.kbdutil.checkElClickable(el.BatteryDoctor["SaveTab"]) is True:
-            self.kbdutil.clickEl(el.BatteryDoctor["SaveTab"])
-            print("[PASS][Home Page]Check Save Tab done.")
-        else:
-            self.assertTrue(False, "[FAIL][Home Page]Check Save Tab fail.")
-
-        if self.kbdutil.checkElVisible(el.SaveTab["Percent"]) is True:
-            HomePagePercent = self.kbdutil.getTextEL(el.SaveTab["Percent"])
-            print("[PASS][Home Page]Get Battery Percentage done. HomePagePercent = " + str(HomePagePercent))
-        else:
-            self.assertTrue(False, "[FAIL][Home Page]Get Battery Percentage fail.")
-
-        if self.kbdutil.scrollTo(el.SaveTab["SwitchWifi"]) is True:
-            print("[PASS][Home Page]Scroll to Get Switch WiFi button done.")
-        else:
-            self.assertTrue(False, "[FAIL][Home Page]Scroll to Get Switch WiFi button fail.")
-
-        if self.kbdutil.checkElVisible(el.SaveTab["SwitchWifi"]) is True:
             subprocess.call("adb shell am start -n com.android.settings/.wifi.WifiStatusTest")
             WiFiStatusA = self.kbdutil.getTextEL(el_other.WifiStatusTest["State"])
             self.driver.back()
-            self.kbdutil.clickEl(el.SaveTab["SwitchWifi"])
+
             subprocess.call("adb shell am start -n com.android.settings/.wifi.WifiStatusTest")
             WiFiStatusB = self.kbdutil.getTextEL(el_other.WifiStatusTest["State"])
+            self.driver.back()
+
             if WiFiStatusB == "Disabled":
-                self.kbdutil.clickEl(el.SaveTab["SwitchWifi"])
-            print("[PASS][Home Page]Test Switch WiFi button done.")
-        else:
-            self.assertTrue(False, "[FAIL][Home Page]Test Switch WiFi button fail.")
+                guide_btn_close = self.kbdutil.waitUntilAndGetElement("id", el.SaveTab["SwitchWifi"], "ClickSwitchWiFiButton", 5)
+                guide_btn_close.click()
+
+            # result = self.kbdutil.checkElVisible(el.SaveTab["FloatingFull"])
+            # self.assertTrue(result, "[FAIL][Home Page] Home page does not have rating dialog after first time execute optimize ")
+            self.assertTrue(False, "Last modified date 2016/10/5.")
+        except:
+            self.kbdutil.screenshot("test_SaveTabHWbutton")
+            self.assertTrue(False)
 
     def test_firstExecute_have_rating(self):
         try:
-            savePowerEle = self.kbdutil.waitUntilAndGetElement("id",el.BatteryDoctor["SaveTab"],"ClickSaveTab",20)
-            #print("[PASS][Home Page]Check Save Tab done.")
+            savePowerEle = self.kbdutil.waitUntilAndGetElement("id", el.BatteryDoctor["SaveTab"], "ClickSaveTab", 20)
             savePowerEle.click()
-            oneTapSavePowerBtnEle = self.kbdutil.waitUntilAndGetElement("id",el.SaveTab["Optimize"],"ClickOptimize")
-            #print("[PASS][Home Page]Click Optimize button done.")
+            oneTapSavePowerBtnEle = self.kbdutil.waitUntilAndGetElement("id", el.SaveTab["Optimize"], "ClickOptimize")
             oneTapSavePowerBtnEle.click()
-            guide_btn_close = self.kbdutil.waitUntilAndGetElement("id",el.ResultPage["PopupWindowX"],"ClickPopupWindowX",5)
-            #print("[PASS][Result Page]Check and Click floating window Tryitnow done. It's 1st time enter to Result Page.")
+            guide_btn_close = self.kbdutil.waitUntilAndGetElement("id", el.ResultPage["PopupWindowX"], "ClickPopupWindowX", 10)
             guide_btn_close.click()
-            backEle = self.kbdutil.waitUntilAndGetElement("id",el.ResultPage["Backkey"],"ClickBackKey")
-            #print("[PASS][Result Page]Click Back key to Home done.")
+            backEle = self.kbdutil.waitUntilAndGetElement("id", el.ResultPage["Backkey"], "ClickBackKey")
             backEle.click()
             result = self.kbdutil.checkElVisible(el.SaveTab["FloatingFull"])
-            self.assertTrue(result,"[FAIL][Home Page] Home page does not have rating dialog after first time execute optimize ")
+            self.assertTrue(result, "[FAIL][Home Page] Home page does not have rating dialog after first time execute optimize ")
         except:
             self.kbdutil.screenshot("firstTimeOptimizeNeedHaveRating")
             self.assertTrue(False)
 
     def test_availableTimecanClickAndGoToItsPage(self):
         try:
-            savePowerEle = self.kbdutil.waitUntilAndGetElement("id",el.BatteryDoctor["SaveTab"],"ClickSaveTab",20)
+            savePowerEle = self.kbdutil.waitUntilAndGetElement("id", el.BatteryDoctor["SaveTab"], "ClickSaveTab", 20)
             savePowerEle.click()
-            test = self.kbdutil.scrollUntilGetElement("id",el.SaveTab["AvailableTimeMovie"],"ClickAvailableTimeArea")
+            test = self.kbdutil.scrollUntilGetElement("id", el.SaveTab["AvailableTimeMovie"], "ClickAvailableTimeArea")
             test.click()
             result = self.kbdutil.checkElVisible(el.Availabletime['2GIcon'])
-            self.assertTrue(result,"[FAIL][Home Page] Click Available time area can not go to Available time page")
+            self.assertTrue(result, "[FAIL][Home Page] Click Available time area can not go to Available time page")
         except:
             self.kbdutil.screenshot("goToAvailableTimePage")
             self.assertTrue(False)
@@ -149,8 +93,11 @@ class KBD_regression_test(unittest.TestCase):
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(KBD_regression_test)
-    #unittest.TextTestRunner(verbosity=2).run(suite)
-    file = open(str(PATH('./KBD_result/' + str(time.strftime("%Y%m%d") + '.html'))) ,"wb")
-    runner = HTMLTestRunner.HTMLTestRunner(stream=file, title="[KBD Automation] [Python+Appium] [Device: " + ' ' + Result["Manufacturer"] + ' ' + Result["Model"] + ' ' + Result["Brand"] + ']', description="[Platform Version: " + Result["Androidversion"] + ']' + '\n' + "[SDK version: " + Result["SDKversion"] + ']' + '\n' + "[Device S/N: " + Result["SerialNo"] + ']')
+    # unittest.TextTestRunner(verbosity=2).run(suite)
+    file = open(str(PATH('./KBD_result/' + str(time.strftime("%Y%m%d") + '.html'))), "wb")
+    runner = HTMLTestRunner.HTMLTestRunner(
+        stream=file,
+        title="[KBD Automation] [Python+Appium] [Device: " + ' ' + Result["Manufacturer"] + ' ' + Result["Model"] + ' ' + Result["Brand"] + ']',
+        description="[Platform Version: " + Result["Androidversion"] + ']' + "[SDK version: " + Result["SDKversion"] + ']' + "[Device S/N: " + Result["SerialNo"] + ']')
     runner.run(suite)
     file.close()
